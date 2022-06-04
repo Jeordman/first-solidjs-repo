dd135d72f51758e1109817c633c04dbc39e34eb6
jeordman <callisterjeordin@gmail.com>
2022-06-04 19:30:38 -0400

    example components added

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,64 @@ function Counter({}) {
   return (
     <>
       <div>{counter()}</div>
-      <button onClick={()=>setCounter(counter()+1)}>Increment</button>
-      <button onClick={()=>setCounter(counter()-1)}>Decrement</button>
+      <button onClick={() => setCounter(counter() + 1)}>Increment</button>
+      <button onClick={() => setCounter(counter() - 1)}>Decrement</button>
     </>
   );
 }
+
+const INITIAL_TODOS = [
+  { text: "Fly to Germany", id: 1 },
+  { text: "Fly to Italy", id: 2 },
+];
 
 function TodoApp({}) {
+  const [todos, setTodos] = createSignal([]);
+  const [newTodoText, setNewTodoText] = createSignal("");
+
+  setTodos(INITIAL_TODOS);
+
+  function handleSubmit() {
+    if (newTodoText() === "") {
+      return;
+    }
+    setTodos([
+      ...todos(),
+      {
+        text: newTodoText(),
+        id: Math.random(),
+      },
+    ]);
+    setNewTodoText("");
+  }
+
+  function handleDelete(id) {
+    setTodos(todos().filter((t) => t.id !== id));
+  }
+
   return (
     <>
-      TODOAPP
+      <ul>
+        <For each={todos()}>
+          {(todo, _) => (
+            <li>
+              {todo.text}
+              <button onClick={() => handleDelete(todo.id)}>Delete</button>
+            </li>
+          )}
+        </For>
+      </ul>
+      <input
+        placeholder="Type here boi"
+        value={newTodoText()}
+        onInput={(e) => setNewTodoText(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleSubmit();
+        }}
+      />
     </>
   );
 }
-
-
 
 function Content() {
   return (
